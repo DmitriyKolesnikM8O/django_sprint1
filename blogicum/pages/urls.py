d734b9bdc44ac740df7dc80095bf668f908ef1ad
Dmitriy <kolesnik982011@gmail.com>
2024-12-08 12:09:14 +0300

    global update

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name: str = 'pages'
+app_name = 'pages'
 
-urlpatterns: list = [
-    path('about/', views.AboutView.as_view(), name='about'),
-    path('rules/', views.RulesView.as_view(), name='rules'),
+urlpatterns = [
+    path('about/', views.about, name='about'),
+    path('rules/', views.rules, name='rules'),
+
 ]
